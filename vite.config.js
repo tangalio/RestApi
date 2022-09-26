678cefc3b1c530de08b3d79f4dabba1ffd75aa62
tangalio <83497105+tangalio@users.noreply.github.com>
2022-09-26 13:28:48 +0700

    setup_reactjs_and_laravel_9

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+export default ({ command }) => ({
+    base: command === 'serve' ? '' : '/build/',
+    publicDir: 'fake_dir_so_nothing_gets_copied',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+        reactRefresh(),
     ],
 });
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
